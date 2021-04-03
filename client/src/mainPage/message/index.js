a12aac828f59a828fa2b742e49b85a3961f30402
Kevin Liu <kevinalexliu@gmail.com>
2021-04-02 23:32:34 -0700

    create banner and textbox components

--- client/src/mainPage/message/index.js
+++ client/src/mainPage/message/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Banner from './components/Banner.js';
+import TextBox from './components/TextBox.js';
 
 const Message = () => {
   return (
-    <h1>I am message</h1>
+    <div>
+      <h1>I am message</h1>
+      <Banner />
+      <TextBox />
+    </div>
   )
 }
 
