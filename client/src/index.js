96cb114de8d5239acf63a391d43647e0ab9bc5ed
Patrick McConnell <pmcconnell13@gmail.com>
2021-04-03 14:00:49 -0700

    Super basic front end skeleton

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginApp from './login/loginApp.js';
+import LoginApp from './login/loginApp.jsx';
 import MainApp from './mainPage/mainApp.js';
 
-ReactDOM.render(window.location.pathname === '/home/' ? <MainApp /> : <LoginApp />, document.getElementById('app')) 
+ReactDOM.render(window.location.pathname === '/home/' ? <MainApp /> : <LoginApp />, document.getElementById('app'))
