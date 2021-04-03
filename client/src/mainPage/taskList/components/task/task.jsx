8e09ee6e8bcfcee0ae52ab255b55c7612c0a427e
Kimberly Lin <kimberlylin2014@gmail.com>
2021-04-03 11:37:21 -0700

    started on task and categoryList

---
+++ client/src/mainPage/taskList/components/task/task.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <form>
+
+      </form>
+    )
+  }
+}
--- client/src/mainPage/taskList/index.js
+++ client/src/mainPage/taskList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/header/header';
+import Header from './components/header/header.jsx';
 
 class TaskListModule extends React.Component {
   constructor(props) {
