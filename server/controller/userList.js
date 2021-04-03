33f809edd8f79bff13cd99a3046b1a29124c7466
Nancy Yang <nyyang.194@gmail.com>
2021-04-03 14:56:33 -0700

    Added route for getting users, displaying on front end.

---
+++ server/controller/userList.js
@@ -0,0 +1,15 @@
+// const db = require('/Users/nancyyang/Desktop/Immersive/cloudCraft/db/config.js')
+
+const db = require('../../db/config.js')
+
+
+exports.getUsers = (req, res) => {
+  db.query('select user_id, name from users;', ((err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  }))
+  // res.send('hitting get users in controller');
+};
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const task_controller = require('./controller/tasks')
+const task_controller = require('./controller/tasks');
+const users_controller = require('./controller/userList');
 
 
 router.get('/tasks', task_controller.getTasks);
+
+router.get('/users', users_controller.getUsers);
 
 module.exports = router
