48c8ed3af29b1085fe2edebe8da7941e9fbdfc5f
Momo Sun <mosun1113@gmail.com>
2021-04-03 23:05:12 -0700

    setup completeTask endpoint

--- server/model/tasks.js
+++ server/model/tasks.js
@@ -38,7 +38,30 @@ const updateTask = (taskId, callback) => {
 //     }
 // })
 
+const completeTask = (task_id, callback) => {
+  db.query('UPDATE tasks SET completed =? WHERE task_id = ?', [true, task_id], (err, results) => {
+    if (err) {
+      callback(err, null);
+      // console.log(err)
+    } else {
+      callback(results.protocol41, null);
+      // console.log(results.protocol41);
+    }
+  });
+};
+
+
+
+// completeTask(2, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(result);
+//     }
+// })
+
 module.exports = {
   getTasks,
   updateTask,
+  completeTask
 };
