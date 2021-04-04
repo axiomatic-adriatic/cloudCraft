32831ea592e524d23369ef34dd9a4baf94abae8d
Kimberly Lin <kimberlylin2014@gmail.com>
2021-04-03 21:42:39 -0700

    added AddTaskFormModal

--- client/src/mainPage/taskList/components/categoryList/categoryList.js
+++ client/src/mainPage/taskList/components/categoryList/categoryList.js
@@ -1,18 +1,73 @@
 import React from 'react';
 import Task from '../task/task';
 import styles from './categoryList.css';
+import AddTaskModalForm from '../addTaskModalForm/addTaskModalForm';
 
-const CategoryList = ({categoryName, tasks, bgColor, textColor, add}) => {
-  const list = tasks.map((task) => <Task task={task} key={task.task_id} />);
-  return (
-    <div className={styles.categoryList}>
-      <h3 style={{backgroundColor: bgColor, color: textColor}}>
-         {categoryName}
-        {add ? <span className={styles.addTask}>&#10133;</span> : null}
-      </h3>
-      <div className={styles.scrollList}>{list}</div>
-    </div>
-  );
-};
+class CategoryList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayModal: 'none',
+    }
+    this.handlOpenModal = this.handlOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleCloseModal(e) {
+    this.setState({
+      displayModal: 'none',
+    })
+  }
+
+  handlOpenModal(e) {
+    this.setState({
+      displayModal: 'block',
+    })
+  }
+
+  render() {
+    const { categoryName, tasks, bgColor, textColor, add } = this.props;
+    const { displayModal } = this.state;
+    const list = tasks.map((task) => <Task task={task} key={task.task_id} />);
+    return (
+      <div className={styles.categoryList}>
+        <h3 style={{ backgroundColor: bgColor, color: textColor }}>
+          {categoryName}
+          {add
+            ? (
+              <span
+                className={styles.addTask}
+                onClick={this.handlOpenModal}
+                onKeyDown={this.handlOpenModal}
+                role="button"
+                tabIndex={0}
+              >
+                &#10133;
+              </span>
+            )
+            : null}
+        </h3>
+        <div className={styles.scrollList}>{list}</div>
+        {add
+          ? (
+            <div id="myModal" className={styles.modal} style={{display: displayModal}}>
+              <div className={styles.modal_content}>
+                <span
+                  className={styles.modal_close}
+                  onClick={this.handleCloseModal}
+                  onKeyDown={this.handleCloseModal}
+                  role="button"
+                  tabIndex={0}
+                >
+                  &times;
+                </span>
+                <AddTaskModalForm />
+              </div>
+            </div>
+          ) : null}
+      </div>
+    );
+  }
+}
 
 export default CategoryList;
