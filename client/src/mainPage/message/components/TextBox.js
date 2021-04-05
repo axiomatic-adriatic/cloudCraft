8eb1ec5819719e947807801d800bec34b8b0ee1a
Kevin Liu <kevinalexliu@gmail.com>
2021-04-05 09:36:44 -0700

    add font awesome, submit functionality, and overall layout

--- client/src/mainPage/message/components/TextBox.js
+++ client/src/mainPage/message/components/TextBox.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
-import { TextArea } from '../styles';
+import { TextArea, Button } from '../styles';
 
-const TextBox = () => {
+const TextBox = ({ submit }) => {
   const [input, setInput] = useState('');
+  const formRef = useRef(null);
   const groupName = 'group one'; // sample
   // const postMessage = (id) => {
   //   axios({
@@ -17,6 +18,13 @@ const TextBox = () => {
   //     .catch((err) => { throw err; });
   // };
 
+  // const onEnterPress = (e) => {
+  //   e.preventDefault();
+  //   if (e.keyCode === 13 && e.shiftKey === false) {
+  //     formRef.submit();
+  //   }
+  // };
+
   const getInput = (e) => {
     e.preventDefault();
     setInput(e.target.value);
@@ -24,12 +32,22 @@ const TextBox = () => {
 
   return (
     <div className="textbox">
-      <TextArea
-        onSubmit={postMessage}
-        placeholder={`Send a message to ${groupName}`}
-        value={input}
-        onChange={getInput}
-      />
+      <form ref={formRef}>
+        <TextArea
+          placeholder={`Send a message to ${groupName}`}
+          value={input}
+          onChange={getInput}
+          // onKeyDown={onEnterPress}
+        />
+        <Button
+          width={4}
+          height={2}
+          size={1}
+          onClick={(e) => submit(e, input)}
+        >
+          &#10148;
+        </Button>
+      </form>
     </div>
   );
 };
