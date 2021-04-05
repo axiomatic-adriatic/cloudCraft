db934ff2beb25331575ddc675203120dbfd7a5cc
Patrick McConnell <pmcconnell13@gmail.com>
2021-04-05 10:37:29 -0700

    auth0 set up

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const router = require('./routes');
 const app = express();
-const port = 3005;
+const port = 3000;
 
 app.use(express.json());
 
