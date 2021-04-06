c005caefa7ee43b7b6bcb4b98ea8b730f50ff5fa
Jeffrey Yeh <nightcrow33@gmail.com>
2021-04-05 17:09:57 -0700

    create new message model

--- server/model/messages.js
+++ server/model/messages.js
@@ -8,6 +8,20 @@ const getMessages = (channel_id) => {
     .catch((error) => error);
 };
 
+/**
+   * create a new message in the table.
+   * @param {Object} message - An object where the keys are column names and
+   * the values are the current values. include channel_id, user_id, message_text
+   * @returns {Promise<Object>} A promise that is fulfilled with an object
+   * containing the results of the query or is rejected with the the error that occurred
+   * during the query.
+ */
+const createMessage = (message) => {
+  const query = 'INSERT INTO messages SET ?';
+  return db.promise().query(query, [message]);
+};
+
 module.exports = {
   getMessages,
+  createMessage,
 };
