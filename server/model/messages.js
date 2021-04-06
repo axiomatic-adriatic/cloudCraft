50d212d0b3809feb4078ebe86d59e59fe39c9d6e
Jeffrey Yeh <nightcrow33@gmail.com>
2021-04-06 15:57:20 -0700

    update message model to also send back sender name

--- server/model/messages.js
+++ server/model/messages.js
@@ -1,8 +1,15 @@
 /* eslint-disable camelcase */
 const db = require('../../db/db.js');
 
+/**
+   * create a new message in the table.
+   * @param {number} channel_id - A number representing the channel id
+   * @returns {Promise<Object>} A promise that is fulfilled with an object
+   * containing the results of the query or is rejected with the the error that occurred
+   * during the query.
+ */
 const getMessages = (channel_id) => {
-  const query = `SELECT * FROM messages WHERE channel_id = ${channel_id} ORDER BY datetime LIMIT 100`;
+  const query = `SELECT messages.*, users.name FROM messages LEFT JOIN users ON users.user_id=messages.user_id WHERE channel_id = ${channel_id} ORDER BY datetime LIMIT 100`;
   return db.promise().query(query)
     .then(([messages]) => messages)
     .catch((error) => error);
