48ec50eabf3f136fc9aa0ed774460a05d3d519cf
Jeffrey Yeh <nightcrow33@gmail.com>
2021-04-08 11:21:32 -0700

    add controller to routes

--- server/controller/messageToTask.js
+++ server/controller/messageToTask.js
@@ -3,10 +3,10 @@ const messages = require('../model/messages');
 const tasks = require('../model/tasks');
 
 const addMessageToTask = (req, res) => {
-  const { user_id, task_text, message_id } = req.body;
+  const { user_id, message_text, message_id } = req.body;
   messages.messageToTask(message_id)
     .then(() => {
-      tasks.addTask(user_id, task_text, message_id, (err, results) => {
+      tasks.addTask(user_id, message_text, message_id, (err, results) => {
         if (err) {
           res.status(500).send(err.message);
         } else {
