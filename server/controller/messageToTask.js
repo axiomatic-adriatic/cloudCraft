d087238f2527d2d9e1d79a2fef63cd5b58945d54
Jeffrey Yeh <nightcrow33@gmail.com>
2021-04-08 11:15:29 -0700

    add message to task

---
+++ server/controller/messageToTask.js
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+const messages = require('../model/messages');
+const tasks = require('../model/tasks');
+
+const addMessageToTask = (req, res) => {
+  const { user_id, task_text, message_id } = req.body;
+  messages.messageToTask(message_id)
+    .then(() => {
+      tasks.addTask(user_id, task_text, message_id, (err, results) => {
+        if (err) {
+          res.status(500).send(err.message);
+        } else {
+          res.status(200).send(results);
+        }
+      });
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
+};
+
+module.exports = {
+  addMessageToTask,
+};
